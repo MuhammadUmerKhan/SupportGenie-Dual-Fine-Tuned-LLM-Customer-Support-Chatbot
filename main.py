278e85d01b765e2406d2354cc28bf79f016006a2
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-03-24 16:55:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
-from frontend.api import chatbot_ui
-from frontend.api import analytics_ui 
+
+# Uncomment these lines to use API instead of direct function calls
+# from frontend.api import chatbot_ui    
+# from frontend.api import analytics_ui  
+
+from frontend.streamlit import chatbot_analytics
 
 # Streamlit Page Config
 st.set_page_config(page_title="AI Customer Support System", page_icon="🤖", layout="wide")
@@ -33,8 +37,10 @@ if page == "🏠 Home":
 
 # Chatbot Page
 elif page == "📱 Chatbot":
-    chatbot_ui.chatbot()
+    # chatbot_ui.chatbot()          # Uncomment these lines to use API instead of direct function calls
+    chatbot_analytics.chatbot()
 
 # Analytics Dashboard Page
 elif page == "📶 Analytics Dashboard":
-    analytics_ui.analytics()
+    # analytics_ui.analytics()      # Uncomment these lines to use API instead of direct function calls
+    chatbot_analytics.analytics()
