27b5e7964c74b881e5980dc00b46b2667dacf841
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-03-25 18:37:02 +0500

    configure mongo db secrets

--- config/config.py
+++ config/config.py
@@ -1,13 +1,20 @@
 import dotenv, os
 from urllib.parse import quote_plus
+
 # Load environment variables
 dotenv.load_dotenv()
 
 GROK_API_KEY = os.getenv("GROK_API_KEY")
 MODEL_NAME = "qwen-2.5-32b"
 FAQ_PATH = "./FAQS/BankFAQs.csv"
-DB_NAME = "chatbotDB"
+DB_NAME = os.getenv("MONGO_DB", "chatbotDB")  # Default to chatbotDB if not set
 FAQ_COLLECTION = "faqs"
 CHAT_HISTORY_COLLECTION = "chat_history"
-encoded_password = quote_plus("MUK546@!")  # Replace this with your actual password
-MONGO_URI = f"mongodb+srv://muhammadumerk546:{encoded_password}@cluster0.tgibo.mongodb.net/chatbotDB?retryWrites=true&w=majority&appName=Cluster0"
+
+# Securely encode MongoDB password
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = quote_plus(os.getenv("MONGO_PASSWORD"))  # Encode special characters
+MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
+
+# Construct MongoDB URI
+MONGO_URI = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/{DB_NAME}?retryWrites=true&w=majority&appName=Cluster0"
