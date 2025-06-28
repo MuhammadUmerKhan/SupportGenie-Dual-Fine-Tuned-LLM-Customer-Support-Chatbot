8926533e0b2f159e7a08f92fb852468377b0b5de
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-06-28 13:59:15 +0500

    del

--- deployment/commands.bash
+++
@@ -1,8 +0,0 @@
-# Navigate to deployment folder (if needed)
-cd deployment
-
-# 1️⃣ Build the Docker Image
-docker build -t ai-customer-support .
-
-# 2️⃣ Run the Container
-docker run -p 8501:8501 ai-customer-support
