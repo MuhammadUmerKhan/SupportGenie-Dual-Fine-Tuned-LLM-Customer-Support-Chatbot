70cc21220d8c721443a48f7074edb2513caf7dac
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-06-28 12:23:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 # from frontend.api import chatbot_ui    
 # from frontend.api import analytics_ui  
 
-from frontend.streamlit import chatbot_analytics
+from frontend.streamlit_files import chatbot_analytics
 
 # Streamlit Page Config
 st.set_page_config(page_title="AI Customer Support System", page_icon="🤖", layout="wide")
